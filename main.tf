dc4f1dfbb469574dff77403bbc733db6a3d050da
Nitro <matriq.mp3@gmail.com>
2026-04-21 19:44:31 +0200

    test ec2

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ resource "aws_instance" "app_server" {
 
   ami           = "ami-0c55b159cbfafe1f0"   # Amazon Linux 2
   instance_type = "t3.medium"
-  subnet_id     = aws_subnet.private_a.id   # referencia na iný resource!
 
   tags = {
     Name = "ecommerce-app"
