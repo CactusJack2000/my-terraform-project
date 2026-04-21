e63d5061204eabae259d3f7185026bafce5f2f6a
Nitro <matriq.mp3@gmail.com>
2026-04-21 19:30:32 +0200

    Add S3 remote state backend with DynamoDB locking

--- provider.tf
+++ provider.tf
@@ -6,6 +6,14 @@ terraform {
     }
   }
   required_version = ">= 1.5.0"
+
+  backend "s3" {
+    bucket         = "my-terraform-state-nitro"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
